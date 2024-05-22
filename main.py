2f20484c8c22348e265aaf9b85557c7c1b8b4f5b
Aleksandr Pokatilov <pokatilov0802@gmail.com>
2024-05-22 12:09:55 +0200

    I don't care about commit messages. I want to sleep

--- main.py
+++ main.py
@@ -23,22 +23,21 @@ print(dev[InquireType.DeviceNumber])
 print(dev[InquireType.DevDMDType])
 print(dev[InquireType.AvailableMemory])
 
-# width = 1024
-# height = 768
 width = dev.width
 height = dev.height
 
-cell = 128
-lg2_c = np.log2(cell)
-assert abs(lg2_c - round(lg2_c)) < 1e-6, "Cell size is not a power of 2"
+x0, x1 = 235, 660
+y0, y1 = 133, 558
+cell = 85
 
 seqs = []
-white = np.ones((1, height, width), dtype=np.uint8)
-for i in range(height // cell):
-    for j in range(width // cell):
-        img = white.copy()
+img = np.ones((1, height, width), dtype=np.uint8)
+for i in range((y1 - y0) // cell):
+    for j in range((x1 - x0) // cell):
+        pattern = np.ones((y1 - y0, x1 - x0), dtype=np.uint8)
+        pattern[cell * i:cell * (i + 1), cell * j:cell * (j + 1)] = 0
 
-        img[0, cell * i:cell * (i + 1), cell * j:cell * (j + 1)] = 0
+        img[0, x0:x1, y0:y1] = pattern
 
         seq = dev.make_sequence(bit_planes=1, pic_num=1)
         seq[SequenceControl.DataFormat] = SequenceValue.DataLSB
@@ -47,15 +46,14 @@ for i in range(height // cell):
 
         seqs.append(seq)
 
-
 for i, seq in enumerate(cycle(seqs)):
     alp42.start_projection(seq, cont=True)
     if i < len(seqs):
         camera.start()
-        camera.take_picture(f"imgs/pixel_size {cell};row {i // 8 + 1};column {i % 8 + 1}.png")
+        camera.take_picture(f"imgs/pixel_size {cell};row {i // ((y1 - y0) // cell) + 1};column {i % ((y1 - y0) // cell) + 1}.png")
     else:
         command = input()
         if command.startswith('q'):
             break
         print("\033[A\033[0K", end='')
-        
+
